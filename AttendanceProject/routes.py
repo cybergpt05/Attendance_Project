2eaf164aff02d7ceb96a0798a07b91a5bfab097c
momhaidat <mjalmousa2005@gmail.com>
2025-03-08 22:15:05 +0300

    Update routes.py

--- AttendanceProject/routes.py
+++ AttendanceProject/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template,url_for,flash,redirect,request,abort,jsonify,Response
-from AttendanceProject import app,db,mail
+from AttendanceProject import app,db
 from AttendanceProject.models import User,Course,Attendance,QRToken
 from flask_login import login_user,logout_user,current_user,login_required
 from werkzeug.security import generate_password_hash, check_password_hash
