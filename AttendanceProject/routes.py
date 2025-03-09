4e11654a35093828e9d959d5df7efd1f23440a4b
momhaidat <mjalmousa2005@gmail.com>
2025-03-09 14:54:40 +0300

    Update routes.py

--- AttendanceProject/routes.py
+++ AttendanceProject/routes.py
@@ -39,8 +39,10 @@ def generate_qr(course_id):
 @app.route("/")
 @app.route("/home",methods=["GET"])
 def home():
-    db.create_all()
-    print(os.getenv('DATABASE_URL'))
+    password = generate_password_hash('$Ju005Admin#juedujo')
+    user = User(account_type='admin',email="juadmin@juadmin.edu.jo",password=password,first_name="Admin",last_name="Account",uni_number="0505888")
+    db.session.add(user)
+    db,session.commit()
     return render_template("home.html",title="Home Page")
 
 @app.route('/login',methods=["GET","POST"])
