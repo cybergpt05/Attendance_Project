febb4ab41ecd6e4f28dec0051dbb209bdf50056e
momhaidat <mjalmousa2005@gmail.com>
2025-03-09 17:31:25 +0300

    Update script.js

--- AttendanceProject/static/script.js
+++ AttendanceProject/static/script.js
@@ -15,7 +15,12 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(data => {
                 if (data.success) {
                     this.textContent = "✔ تم تسجيل الحضور";  
-                    this.disabled = true;  
+                    this.disabled = true;
+                    const button = document.querySelector(`button[data-student-id="${studentId}"].mark-absent-btn`);
+                    if (button) {
+                        button.removeAttribute('disabled');
+                        button.textContent = 'تسجيل الغياب ';
+                    }
                     let statusSpan = document.querySelector(`#attendance-status-${studentId}`);
                     if (statusSpan) {
                         statusSpan.textContent = "حاضر";
@@ -23,6 +28,42 @@ document.addEventListener("DOMContentLoaded", function () {
                     }
                 } else {
                     alert("حدث خطأ أثناء تسجيل الحضور!");
+                }
+            })
+            .catch(error => console.error("Error:", error));
+        });
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".mark-absent-btn").forEach(button => {
+        button.addEventListener("click", function () {
+            let studentId = this.dataset.studentId; 
+            let courseId = this.dataset.courseId;
+            
+            fetch("/mark_absent", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ student_id: studentId, course_id: courseId })
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    this.textContent = "✔ تم تسجيل الغياب";  
+                    this.disabled = true;
+                    const button = document.querySelector(`button[data-student-id="${studentId}"].mark-attendance-btn`);
+                    if (button) {
+                        button.removeAttribute('disabled');
+                        button.textContent = 'تسجيل الحضور ';
+                    }
+                    let statusSpan = document.querySelector(`#attendance-status-${studentId}`);
+                    if (statusSpan) {
+                        statusSpan.textContent = "غائب";
+                        statusSpan.style.color = "red";
+                    }
+                } else {
+                    alert("حدث خطأ أثناء تسجيل الغياب!");
                 }
             })
             .catch(error => console.error("Error:", error));
