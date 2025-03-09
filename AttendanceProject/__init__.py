ea8ac34b4e65c09f2d007d50f7168b826b96718d
momhaidat <mjalmousa2005@gmail.com>
2025-03-09 04:11:06 +0300

    Update __init__.py

--- AttendanceProject/__init__.py
+++ AttendanceProject/__init__.py
@@ -3,10 +3,10 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
-
+import os
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "ec29e9c6c1116fe18003de25ffa6805200e8711d42ab0cadce7e85b52d07b8c7"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ju_users_record.db"  
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL', 'sqlite:///ju_users_record.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
