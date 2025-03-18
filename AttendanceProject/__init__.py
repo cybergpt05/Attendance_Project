80244d2247f462f4626f2e102aa57951dcb8d865
momhaidat <mjalmousa2005@gmail.com>
2025-03-18 21:57:51 +0300

    Update __init__.py

--- AttendanceProject/__init__.py
+++ AttendanceProject/__init__.py
@@ -5,10 +5,11 @@ from flask_mail import Mail
 from flask_migrate import Migrate
 import os
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 240
 
 
 db = SQLAlchemy(app)
